27bb18ce3b169340cb41b0269f97b3635dc91058
KarliEST <karlikullasepp@gmail.com>
2023-10-02 01:24:12 +0300

    tables

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,11 +1,11 @@
-create sequence locations_id_seq
+create schema public;
+
+create sequence public.locations_id_seq
     as integer;
 
-alter sequence locations_id_seq owner to postgres;
-
-create table location
+create table public.location
 (
-    id          integer default nextval('locations_id_seq'::regclass) not null
+    id          integer default nextval('public.locations_id_seq'::regclass) not null
         constraint locations_pkey
             primary key,
     longitude   double precision                                      not null,
@@ -13,7 +13,4 @@ create table location
     description varchar
 );
 
-alter table location
-    owner to postgres;
-
-alter sequence locations_id_seq owned by location.id;
+alter sequence public.locations_id_seq owned by location.id;
